8068493c73b26671c900ba422ae3b2b2c1287f2d
ruibin.chow <ruibin.chow@qq.com>
2026-01-14 15:18:01 +0800

    Update tongji.

--- vender/js/tongji.js
+++ vender/js/tongji.js
@@ -10,10 +10,8 @@ var _hmt = _hmt || [];
 (function() {
     if(window.location.host.indexOf("localhost") == -1){
         var hm = document.createElement("script");
-        // hm.src = "https://hm.baidu.com/hm.js?581b72d4667d80609e3240d190f1e43a";
-        hm.src = "https://hm.baidu.com/hm.js?0fe5e1a8e525e789a3326bfee758792d"; //zruibin
+        hm.src = "https://hm.baidu.com/hm.js?1cd35ecc8aad6ef12a19651a878ff628";
         var s = document.getElementsByTagName("script")[0]; 
         s.parentNode.insertBefore(hm, s);
-        // console.log(sourceFile);
     }
 })();
